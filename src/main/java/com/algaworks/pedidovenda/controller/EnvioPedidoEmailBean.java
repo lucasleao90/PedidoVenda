1c1fc21cee0762415d2b0df04095bdf5ca387544
lucas.pimenta <lucasleao90@hotmail.com>
2017-10-19 16:38:58 -0300

    modulo 14.4

--- src/main/java/com/algaworks/pedidovenda/controller/EnvioPedidoEmailBean.java
+++ src/main/java/com/algaworks/pedidovenda/controller/EnvioPedidoEmailBean.java
@@ -1,15 +1,19 @@
 package com.algaworks.pedidovenda.controller;
 
 import java.io.Serializable;
+import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.velocity.tools.generic.NumberTool;
+
 import com.algaworks.pedidovenda.model.Pedido;
 import com.algaworks.pedidovenda.util.jsf.FacesUtil;
 import com.algaworks.pedidovenda.util.mail.Mailer;
 import com.outjected.email.api.MailMessage;
+import com.outjected.email.impl.templating.velocity.VelocityTemplate;
 
 @Named
 @RequestScoped
@@ -29,7 +33,10 @@ public class EnvioPedidoEmailBean implements Serializable {
 		
 		message.to(this.pedido.getCliente().getEmail())
 			.subject("Pedido " + this.pedido.getId())
-			.bodyHtml("<strong>Valor total:</strong> " + this.pedido.getValorTotal())
+			.bodyHtml(new VelocityTemplate(getClass().getResourceAsStream("/emails/pedido.template")))
+			.put("pedido", this.pedido)
+			.put("numberTool", new NumberTool())
+			.put("locale", new Locale("pt", "BR"))
 			.send();
 		
 		FacesUtil.addInfoMessage("Pedido enviado por e-mail com sucesso!");
